ff7ae7a8335779141d4a56fb4e01f7a191dc0dc4
hieupx <hieubrianpham@gmail.com>
2017-12-19 11:38:59 +0700

    first commit

--- src/main/java/com/homecare/HomecareConfigClientApplication.java
+++ src/main/java/com/homecare/HomecareConfigClientApplication.java
@@ -3,32 +3,16 @@ package com.homecare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @author zhao
  */
 
 @SpringBootApplication
-@EnableEurekaClient
-@RestController
-@RefreshScope
+@EnableConfigServer
 public class HomecareConfigClientApplication {
-
     public static void main(String[] args) {
-
         SpringApplication.run(HomecareConfigClientApplication.class, args);
-    }
-
-    @Value("${logging.level.com.didispace}")
-    String foo;
-
-    @RequestMapping(value = "/hi")
-    public String hi(){
-        return foo;
     }
 }
